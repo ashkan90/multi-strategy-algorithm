8ac9e1937cf546c5ed4202c9d94e24b66a0cf77e
ashkan90 <Krhncddm12>
2021-02-08 14:07:11 +0300

    fixed >>> nil pointer on hooks when they're not called

--- communication/algorithms/email.go
+++ communication/algorithms/email.go
@@ -15,7 +15,9 @@ type Email struct {
 }
 
 func (e *Email) Send() {
-	e.before(e)
+	if e.before != nil {
+		e.before(e)
+	}
 
 	smtp := driver.NewSMTP(e.URI, e.Headers)
 	smtp.Serialize(e.Params)
@@ -26,20 +28,16 @@ func (e *Email) Send() {
 		return
 	}
 
-	e.after(smtp.Response)
+	if e.after != nil {
+		e.after(smtp.Response)
+	}
 
 	log.Println("EmailStrategy -> Response -> ", smtp.Response)
 }
 
 func (e *Email) Before(handler strategy.BeforeHandler) {
-	if handler == nil {
-		handler = func(algorithm strategy.Algorithm) {}
-	}
 	e.before = handler
 }
 func (e *Email) After(handler strategy.AfterHandler) {
-	if handler == nil {
-		handler = func(response interface{}) {}
-	}
 	e.after = handler
 }
